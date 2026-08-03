dd672976969b9626b991e84c04c1b2ebf386851c
Harshith Ashok <50227707+harshith-ashok@users.noreply.github.com>
2026-08-03 23:12:59 +0530

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sort = ''.join(sorted(s))
+            res[sort].append(s)
+
+        return list(res.values())
